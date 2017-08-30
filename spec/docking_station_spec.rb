1df3fd7095ebf9cd9e952f2f62cfa4ea106ccfab
Luan van Pletsen <luanvanpletsen@msn.com>
2017-08-30 16:10:04 +0100

    station releases working bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,16 +5,15 @@ describe DockingStation do
   it 'responds to release_bike' do
     expect(subject).to respond_to :release_bike
   end
-  # it 'return_bike returns a bike' do
-  #   expect(subject.release_bike.is_a? Bike).to eq true
-  # end
-  it 'return_bike returns a bike' do
-    expect(subject.release_bike).to be_an_instance_of(Bike)
+
+
+
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_an_instance_of(Bike)
+    expect(bike).to be_working
   end
-  it 'checks bike is working' do
-    expect(subject.release_bike.working?).to eq true
-  end
-  # it { should be_a(Bike)}
+
 end
 
 # docking_station.release_bike.is_a? Bike
