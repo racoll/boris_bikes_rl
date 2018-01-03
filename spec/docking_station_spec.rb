69f7103aee7dcd7fece54e081a7a246c5e87a193
robincollins <racollins1@me.com>
2018-01-03 17:25:25 +0000

    raising capacity error

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -13,7 +13,7 @@ describe DockingStation do
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
-  it { is_expected.to respond_to :bike }
+  it { is_expected.to respond_to(:bike) }
 
   it "docks something" do
     bike = Bike.new
@@ -24,6 +24,19 @@ describe DockingStation do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
+  end
+
+  describe "#release_bike" do
+
+    it "releases a bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it "raises an error when there are no bikes available" do
+      expect { subject.release_bike }.to raise_error "No bikes available"
+    end
   end
 
 
